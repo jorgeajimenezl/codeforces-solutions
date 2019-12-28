0160591a8257f8bcce25a37f987caa3922b84034
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2019-12-28 17:21:00 +0000

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/67808112

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    //freopen("a.in", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int h, m;
+        cin >> h >> m;
+
+        cout << (23 - h) * 60 + (60 - m) <<'\n';
+    }
+}
