fa5efd5722b753ac3641480a1f5011268b4a64bf
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2020-04-19 22:05:00 +0000

    Add solution for problem `D - Yet Another Array Queries Problem` Link: https://codeforces.com/contest/863/problem/D Tags: data structures, implementation, *1800 Ref: https://codeforces.com/contest/863/submission/77314851

--- codeforces/863/D.cpp
+++ codeforces/863/D.cpp
@@ -5,21 +5,15 @@ using namespace std;
 struct node
 {
     int value;
-    int64_t sum, lazy;
     int size, prio;
     node *l, *r;
     bool rev;
     node (int value) : value(value), prio(rand()), 
-                        sum(0), lazy(0),
                         rev(false), l(nullptr), r(nullptr) { }
 };
 
 int size (node* u) {
     return u ? u->size : 0;
-}
-
-int64_t sum (node* u) {
-    return u ? u->sum : 0;
 }
 
 void push (node* u) {
@@ -30,21 +24,11 @@ void push (node* u) {
         if (u->l)  u->l->rev = !u->l->rev;
         if (u->r)  u->r->rev = !u->r->rev;
     }
-
-    if (u && u->lazy) {
-        u->value += u->lazy;
-        
-        if (u->l) u->l->lazy += u->lazy;
-        if (u->r) u->r->lazy += u->lazy;
-
-        u->lazy = 0;
-    }
 }
 
 node* update (node* u) {
     if (u) {
         u->size = 1 + size(u->l) + size(u->r);
-        u->sum = u->value + sum(u->l) + sum(u->r);
     }
 
     return u;
@@ -130,36 +114,16 @@ void reverse (node* &u, int l, int r) {
     u = merge(merge(t.first, p.first), p.second);
 }
 
-// int get (node* u, int i) {
-//     if (u) {
-//         push(u);
+int get (node* u, int i) {
+    if (u) {
+        push(u);
 
-//         if (size(u->l) == i)
-//             return u->value;
-//         else
-//             return get((size(u->l) >= i) ? u->l : u->r, 
-//                         (size(u->l) >= i) ? i : i - size(u->l) - 1);
-//     }
-// }
-
-int64_t query (node* &u, int l, int r) {
-    push(u);
-    
-    auto t = split(u, l);
-    auto p = split(t.second, r - l + 1);
-    int ans = p.first ? p.first->sum : 0;
-    u = merge(merge(t.first, p.first), p.second);
-
-    return ans;
-}
-
-void update_interval (node* &u, int l, int r, int x) {
-    push(u);
-    
-    auto t = split(u, l);
-    auto p = split(t.second, r - l + 1);
-    if (p.first) p.first->lazy = x;
-    u = merge(merge(t.first, p.first), p.second);
+        if (size(u->l) == i)
+            return u->value;
+        else
+            return get((size(u->l) >= i) ? u->l : u->r, 
+                        (size(u->l) >= i) ? i : i - size(u->l) - 1);
+    }
 }
 
 int main() {
@@ -186,7 +150,7 @@ int main() {
         if (t == 2)
             reverse(root, a, b);
         else {
-            int x = query(root, b, b);
+            int x = get(root, b);
             erase(root, b);
             insert(root, a, x);
         }        
@@ -195,6 +159,6 @@ int main() {
     while (m--) {
         int pos;
         cin >> pos;
-        cout << query(root, pos, pos) << " ";
+        cout << get(root, pos) << " ";
     }
 }
