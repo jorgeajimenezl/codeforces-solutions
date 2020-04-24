2320be63c22b5d31d1dd1e02012e6f5f144f5d85
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2020-04-24 20:08:00 +0000

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/77953811

---
+++ codeforces/1337/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+#if _DEBUG
+    freopen("a.in", "r", stdin);
+#endif
+
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int64_t a, b, c, d;
+        cin >> a >> b >> c >> d;        
+        cout << b << " " << c << " " << c << "\n";
+    }
+}
