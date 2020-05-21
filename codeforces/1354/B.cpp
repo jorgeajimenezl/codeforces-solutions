a0ab8c658a442b0c7aac5c3a1e17260bfc46c6c3
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2020-05-21 11:55:00 +0000

    Add solution for problem `B - Ternary String` Link: https://codeforces.com/contest/1354/problem/B Tags: binary search, dp, implementation, two pointers, *1200 Ref: https://codeforces.com/contest/1354/submission/80848793

--- codeforces/1354/B.cpp
+++ codeforces/1354/B.cpp
@@ -1,14 +1,15 @@
-
 #include <bits/stdc++.h>
+#define debug(x) cerr << #x << ": " << x << endl;
+
 using namespace std;
 
-// 💖 Hi, thanks for using ICIE! 💖
-// 🔧 To use a custom code template, set it in Settings(Ctrl+,) in "Icie Template List" entry 🔧
-// 📝 If you spot any bugs or miss any features, create an issue at https://github.com/pustaczek/icie/issues 📝
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+#if _DEBUG
+    freopen("a.in", "r", stdin);
+#else
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+#endif
 
     int T;
     cin >> T;
@@ -16,29 +17,29 @@ int main() {
     while (T--) {
         string s;
         cin >> s;
- 
+
         int n = s.length();
- 
+
         vector<vector<int>> dp(s.size() + 1, vector<int>(5));
- 
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 3; j++)
                 dp[i][j] += dp[i - 1][j];
             
             dp[i][s[i - 1] - '0']++;
         }
- 
+
         auto query = [dp](int a, int b) {
             for (int i = 1; i <= 3; i++)
                 if (dp[b][i] - dp[a - 1][i] == 0)
                     return false;
             return true;
         };
- 
+
         int ans = 1 << 30;
         for (int i = 1; i <= n; i++) {
             int lo = i, hi = n;
- 
+
             while (lo < hi) {
                 int m = (lo + hi) >> 1;
                 if (query(i, m))
@@ -46,16 +47,15 @@ int main() {
                 else
                     lo = m + 1;
             }
- 
+
             if (!query(i, lo))
                 lo++;
                 
             if (lo <= n && query(i, lo))
                 ans = min(ans, lo - i + 1);
         }
- 
+
         cout << (ans != 1 << 30 ? ans : 0) << "\n";
     }
-
 }
 
