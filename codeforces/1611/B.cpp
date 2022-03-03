fa871ff5e0edb9c855bfb7932637f9e6101b3840
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2022-03-03 20:18:00 +0000

    Add solution for problem `B - Team Composition: Programmers and Mathematicians` Link: https://codeforces.com/contest/1611/problem/B Tags: binary search, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1611/submission/148235745

---
+++ codeforces/1611/B.cpp
@@ -0,0 +1,47 @@
+// Coder: jorgeajimenezl
+#include <bits/stdc++.h>
+// #pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+#if _DEBUG
+#define debug(x) cerr << #x << ": " << x << endl;
+#pragma GCC optimize("trapv")
+#else
+#define debug(x)
+#endif
+#define max(a, b) ((a > b) ? a : b)
+#define min(a, b) ((a < b) ? a : b)
+
+template<typename T>
+ostream& operator<< (ostream& os, const vector<T> &a) {
+    for (size_t i = 0; i < a.size() - 1; i++)
+        os << a[i] << " ";
+    return (os << a.back());
+}
+
+int main() {
+#if _DEBUG
+    freopen("a.in", "r", stdin);
+#else
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+#endif
+    
+    int T = 1;
+    cin >> T;
+
+    while (T--) {
+        int64_t a, b;
+        cin >> a >> b;
+
+        if ((a + b) / 4 <= min(a, b))
+            cout << (a + b) / 4 << "\n";
+        else 
+            cout << min(a, b) << "\n";
+    }
+
+#if _DEBUG
+    cout << "Time elapsed: " << double(clock()) / double(CLOCKS_PER_SEC) << "s\n";
+#endif
+}
